8354d9fa887960764691e2e0e02e68cfe361d9c3
Louis <188152116@qq.com>
2026-06-30 20:23:25 +0800

    add milkdown widgets to dacap cms

---
+++ source/widgets/milkdown/milkdown-widget.js
@@ -0,0 +1,23 @@
+const MilkdownControl = class extends React.Component {
+  constructor(props) {
+    super(props)
+    this.ref = React.createRef()
+  }
+
+  componentDidMount() {
+    this.editor = window.initMilkdown(
+      this.ref.current,
+      this.props.value || ''
+    )
+  }
+
+  componentWillUnmount() {
+    this.editor?.destroy?.()
+  }
+
+  render() {
+    return <div ref={this.ref} />
+  }
+}
+
+CMS.registerWidget('milkdown', MilkdownControl)
